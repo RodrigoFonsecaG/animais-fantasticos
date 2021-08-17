a106f0df56fb428707b486e23a2776094393b28c
RodrigoFonsecaG <rodrigofon1@hotmail.com>
2021-08-17 09:59:09 -0300

    Tooltip using class

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@ import ScrollSuave from './modules/scroll-suave.js';
 import Accordion from './modules/accordion.js';
 import TabNav from './modules/tabnav.js';
 import Modal from './modules/modal.js';
-import initTooltip from './modules/tooltip.js';
+import Tooltip from './modules/tooltip.js';
 import initDropdownMenu from './modules/dropdown-menu.js';
 import initMenuMobile from './modules/menu-mobile.js';
 import initFuncionamento from './modules/funcionamento.js';
@@ -16,14 +16,22 @@ scrollSuave.init();
 const accordion = new Accordion('[data-anime="accordion"] dt');
 accordion.init();
 
-const tabNav = new TabNav('[data-tab="menu"] li','[data-tab="content"] section');
+const tabNav = new TabNav(
+  '[data-tab="menu"] li',
+  '[data-tab="content"] section'
+);
 tabNav.init();
 
-const modal = new Modal('[data-modal="open"]','[data-modal="close"]', '[data-modal="container"]');
+const modal = new Modal(
+  '[data-modal="open"]',
+  '[data-modal="close"]',
+  '[data-modal="container"]'
+);
 modal.init();
 
+const tooltip = new Tooltip('[data-tooltip]');
+tooltip.init();
 
-initTooltip();
 initDropdownMenu();
 initMenuMobile();
 initFuncionamento();
